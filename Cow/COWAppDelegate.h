35a986537ccb7614ab499bc11a19f100483f2f36
Simon Hau <simon@simon-macbook.local>
2011-11-12 16:13:01 +0100

    Initial Commit

---
+++ Cow/COWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  COWAppDelegate.h
+//  Cow
+//
+//  Created by Simon Hau on 12/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface COWAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Cow/COWAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  COWAppDelegate.m
+//  Cow
+//
+//  Created by Simon Hau on 12/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "COWAppDelegate.h"
+
+@implementation COWAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
